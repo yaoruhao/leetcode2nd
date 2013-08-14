5fff9818dd34de5fdbc45aa37647234113528e8f
Ruhao Yao <yaoruhao@gmail.com>
2013-08-14 15:46:48 +0800

    Bugfix:MaximalRectangle

--- MaximalRectangle.java
+++ MaximalRectangle.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * 
  * @author yaoruhao
@@ -11,37 +13,50 @@ public class MaximalRectangle {
         if (matrix.length == 0 || matrix[0].length == 0) {
 			return 0;
 		}
-        int n = matrix[0].length;
-        int []heights = new int [n];
-        int []lefts = new int [n];
-        int []rights = new int [n];
-        for (int i = 0; i < rights.length; i++) {
-			rights[i] = n;
-		}
-        int result = 0;
-        for (int i = 0; i < matrix.length; i++) {
-			int left = 0;
-			int right = n;
-			for (int j = 0; j < n; j++) {
-				if (matrix[i][j] == '1') {
-					heights[j]++;
-					lefts[j] = Math.max(left, lefts[j]);
-				} else {
-					heights[j] = 0;
-					left = j + 1;
-					lefts[j] = 0;
-					rights[j] = n;
-				}
-			}
-			for (int j = n - 1; j >= 0; j--) {
-				if (matrix[i][j] == '1') {
-					rights[j] = Math.min(rights[j], right);
-					result = Math.max(result, heights[j] * (rights[j] - lefts[j]));
-				} else {
-					right = j;
-				}
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int [][] lenTable = new int [rows][columns];
+        for (int i = 0; i < columns; i++) {
+			if (matrix[0][i] == '1') {
+				lenTable[0][i] = 1;
 			}
 		}
-        return result;
+        for (int i = 1; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (matrix[i][j] == '0') {
+					continue;
+				}
+				lenTable[i][j] = lenTable[i-1][j] + 1;
+			}
+		}
+        int maxArea = 0;
+        for (int i = 0; i < rows; i++) {
+			maxArea = Math.max(maxArea, maximalHistogram(lenTable[i]));
+		}
+        return maxArea;
+    }
+    
+    public int maximalHistogram(int []histogram) {
+    	int maxArea = 0;
+    	Stack<Integer> stack = new Stack<Integer>();
+    	for (int i = 0; i < histogram.length; i++) {
+			if (stack.isEmpty() || histogram[stack.peek()] < histogram[i]) {
+				stack.push(i);
+			} else {
+				int lastIndex = stack.pop();
+				int width = i;
+				if (!stack.isEmpty()) {
+					width = i - stack.peek() - 1;
+				}
+				maxArea = Math.max(maxArea, histogram[lastIndex] * width);
+				i--;
+			}
+		}    	
+    	while (!stack.isEmpty()) {
+			int lastIndex = stack.pop();
+			int width = stack.isEmpty() ? histogram.length : histogram.length - stack.peek() - 1;
+			maxArea = Math.max(maxArea, histogram[lastIndex] * width);
+		}    	
+    	return maxArea;
     }
 }
